04f89681cf76bd1cb092a293dda1f74e010275bd
unknown <a.sauvinet@FR-D-050.be-ad.loc>
2015-06-17 18:08:54 +0200

    initial commit

---
+++ BuildMonitor.WPF/App/Models/Model.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App.Models
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+    }
+
+    public class ProjectBuild
+    {
+        public Project Project { get; set; }
+        public DateTime Start { get; set; }
+        public Int64 Time { get; set; }
+    }
+
+    public class Solution
+    {
+        public String Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class Build
+    {
+        public DateTime Start { get; set; }
+        public Int64 Time { get; set; }
+        public Solution Solution { get; set; }
+        public IEnumerable<Project> Projects { get; set; }
+    }
+}
+
+/*
+[{
+  "Start": "2015-06-17T10:53:32.6920896+02:00",
+  "Time": 84765,
+  "Solution": {
+    "Name": "Xamarin.Sport"
+  },
+  "Projects": [
+    {
+      "Start": "2015-06-17T10:53:33.3676877+02:00",
+      "Time": 16,
+      "Project": {
+        "Name": "Sport.Core.Enumerations.Portable",
+        "Id": "23ca8915-038a-4470-ae18-bc7ded7549e3"
+      }
+    },
+ 
+*/
---
+++ BuildMonitor.WPF/App/Services/BuildRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using App.Models;
+using Newtonsoft.Json;
+
+namespace App.Services
+{
+    public class BuildRepository
+    {
+        public async Task<IEnumerable<Build>> GetAll()
+        {
+            var bulk = await new BuildFileReader().Read();
+
+            var builds = JsonConvert.DeserializeObject<IEnumerable<Build>>(bulk);
+            return builds;
+        }
+    }
+
+    public class BuildFileReader
+    {
+        public async Task<String> Read()
+        {
+            var path = "C:/Users/a.sauvinet/AppData/Roaming/Build Monitor/buildtimes.json";
+
+            using (var reader = File.OpenText(path))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
